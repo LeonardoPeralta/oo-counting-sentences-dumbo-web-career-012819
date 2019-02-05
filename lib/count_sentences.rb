5501c298d865dfb6183b075279de40f4f399ee2d
Leonardo Peralta <leo.pm15@gmail.com>
2019-02-05 16:11:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,9 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    new_array = self.split(/[.?!]/).reject do |string|
+      string.empty?
+    end
       return new_array.length
   end
 end
